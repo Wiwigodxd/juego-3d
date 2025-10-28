217df290af834f3d4c013d1a3b95275ff96e2c8b
Wiwigodxd <edwin.sanchez06@inacapmail.cl>
2025-10-28 17:34:46 -0300

    feat : andamos de pana

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,17 @@ extends CharacterBody3D
 
 @export var speed = 14
 @export var fall_acceleration = 75
+@export var bounce_impulse = 16
+@export var jump_impulse = 20
 
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+
 	
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
@@ -29,5 +35,17 @@ func _physics_process(delta):
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
 	velocity = target_velocity
+	
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+		if collision.get_collider() == null:
+			continue
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				break
+				
 	move_and_slide()
 	
